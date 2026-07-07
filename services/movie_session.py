04ce556c0b2891ec61ab7f0a50591d69c81168f3
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-07-08 00:03:18 +0300

    Fixed flake8 #2

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,6 @@ def create_movie_session(
         cinema_hall_id: int
 ) -> MovieSession:
 
-
     new_movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
